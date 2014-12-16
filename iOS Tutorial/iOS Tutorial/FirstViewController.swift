17ff80e07be1ddb54a241410cd38973b5b78bbe6
Bas Broek <bas@basbroek.nl>
2014-12-16 19:43:44 +0100

    Initial commit / start

---
+++ iOS Tutorial/iOS Tutorial/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  iOS Tutorial
+//
+//  Created by Bas Broek on 16/12/14.
+//  Copyright (c) 2014 Bas Broek. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
